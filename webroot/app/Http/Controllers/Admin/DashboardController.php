c8f56e639f0cea5b581f1cb5afa5542c355ce146
alex s <sanatateacom@gmail.com>
2021-08-28 10:08:51 +0300

    display statistics on dashboard

--- webroot/app/Http/Controllers/Admin/DashboardController.php
+++ webroot/app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Models\Recruit;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,6 +13,10 @@ class DashboardController extends Controller
 {
     public function home(): View
     {
-        return view('dashboard');
+        $totalUsers = User::query()->count();
+        $totalCompanies = Company::query()->count();
+        $totalRecruits = Recruit::query()->count();
+
+        return view('dashboard', compact('totalUsers', 'totalCompanies', 'totalRecruits'));
     }
 }
