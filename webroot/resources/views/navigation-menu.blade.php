2ecd5ce4ef7436576820c3ae65be0355eb200ae5
alex s <sanatateacom@gmail.com>
2021-08-29 23:15:28 +0300

    change logo app

--- webroot/resources/views/navigation-menu.blade.php
+++ webroot/resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <x-jet-application-mark class="block h-9 w-auto rounded-full" />
                     </a>
                 </div>
 
